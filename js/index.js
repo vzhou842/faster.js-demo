2a796e7f8d5968f6f43e81208f5434e2b00c2f27
Victor Zhou <vzhou842@gmail.com>
2018-04-10 16:24:37 -0400

    Added statistics + border color via .reduce()

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const height = canvas.height;
 
 // Initialize particles
 let particles = [];
-const NUM_PARTICLES = 1000;
+const NUM_PARTICLES = 5000;
 for (let i = 0; i < NUM_PARTICLES; i++) {
 	addParticle();
 }
@@ -27,20 +27,36 @@ function addParticle() {
 
 // Renders the particles
 function render() {
-	context.fillStyle = 'black';
+	context.fillStyle = 'rgba(0, 0, 0, 0.25)';
 	context.fillRect(0, 0, width, height);
 
 	// Render each particle
 	particles.forEach(p => p.render(context));
 
+	// Choose the border color based on the average of all particle colors
+	let color = particles.reduce((acc, p) => {
+		acc[0] += p.r;
+		acc[1] += p.g;
+		acc[2] += p.b;
+		return acc;
+	}, [0, 0, 0]);
+	color = color.map(c => {
+		const avg = Math.round(c / particles.length);
+		return Math.min(255, Math.max(0, 128 + (avg - 128) * 10));
+	});
+	context.strokeStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+	context.lineWidth = 6;
+	context.strokeRect(3, 3, width - 6, height - 6);
+
 	// Render the update time
 	context.fillStyle = 'black';
-	context.fillRect(0, 0, 70, 25);
+	context.fillRect(0, 0, 88, 44);
 	context.fillStyle = 'white';
 	context.font = '13px Helvetica';
 	context.textBaseline = 'top';
 	context.textAlign = 'left';
-	context.fillText(`${avgUpdateTime.toFixed(2)} ms`, 5, 5);
+	context.fillText(`Render ${updateCount}`, 5, 5);
+	context.fillText(`${avgUpdateTime.toFixed(2)} ms`, 5, 24);
 }
 
 // Updates the simulation state
@@ -71,7 +87,14 @@ function update() {
 
 	updateCount++;
 
+	// Record statistics if needed
+	if (updateCount === 100 || updateCount === 500 || updateCount === 1000) {
+		const t = Math.round(avgUpdateTime * updateCount);
+		const label = document.getElementById(`first-${updateCount}-label`);
+		label.textContent = `${updateCount} renders: ${t} ms (${avgUpdateTime.toFixed(1)} ms/render)`;
+	}
+
 	requestAnimationFrame(update);
 }
 
-update();
+requestAnimationFrame(update);
